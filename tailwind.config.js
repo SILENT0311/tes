bf6564ecb067ca3b8dd645406c87b09d51082ba9
SILENT0311 <putramarsel12@gmail.com>
2024-03-14 12:21:44 +0700

    create ojo lali

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./src/**/*.{js,ts,jsx,tsx,css}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'poppins': ['Poppins', 'Arial', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
+
